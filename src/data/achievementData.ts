48caf27c87218451fe7820b9a5c28d0e57601193
kade <58316992+kadezaccaro@users.noreply.github.com>
2026-07-17 23:20:45 -0400

    Generate achievement cards dynamically

---
+++ src/data/achievementData.ts
@@ -0,0 +1,17 @@
+import GradIcon from "../components/icons/GradIcon.astro";
+import BadgeIcon from "../components/icons/BadgeIcon.astro";
+
+export const achievementData = [
+  {
+    title: "Associate's Degree in Cybersecurity",
+    issuer: "Lanier Technical College",
+    date: "Expected December 2026",
+    icon: GradIcon,
+  },
+  {
+    title: "CompTIA A+ Certification",
+    issuer: "CompTIA",
+    date: "Expected May 2026",
+    icon: BadgeIcon,
+  },
+];
